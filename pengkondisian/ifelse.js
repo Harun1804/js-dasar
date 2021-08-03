4bc871431249593a3caf4c9583869649e7840b23
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-08-03 09:57:01 +0700

    Add ElseIf

--- pengkondisian/ifelse.js
+++ pengkondisian/ifelse.js
@@ -1,7 +1,9 @@
 var angka = prompt('Masukan Angka');
 
-if(angka % 2 == 0){
+if(angka % 2 === 0){
   alert(angka + " Adalah bilangan genap");
-}else{
+}else if(angka % 2 === 1){
   alert(angka + " Adalah bilangan ganjil");
+}else{
+  alert(angka + " Bukan Angka !!!");
 }
