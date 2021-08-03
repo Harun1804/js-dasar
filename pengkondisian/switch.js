33ac3a3a9acc8824a0a410be2492b586ca1abcc5
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-08-03 10:28:54 +0700

    Pengondisian Switch

---
+++ pengkondisian/switch.js
@@ -0,0 +1,10 @@
+var angka = parseInt(prompt("Masukan Angka"));
+
+switch (angka) {
+  case 1:
+      alert("Anda Memasukan Angka 1");
+    break;
+  default:
+      alert("Yang Anda Masukan Tidak Ada");
+    break;
+}
