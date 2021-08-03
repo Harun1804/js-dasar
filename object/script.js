df613f427b324b5867732caf31bf7c734bc2208f
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-08-03 22:34:44 +0700

    Membuat Object

--- object/script.js
+++ object/script.js
@@ -1,3 +1,5 @@
+// Membuat Objeck
+// Objeck Literal
 var mahasiswa = {
     nama : 'Harun Ar - Rasyid',
     umur : 22,
@@ -12,4 +14,25 @@ var mahasiswa = {
     }
 }
 
-console.log(mahasiswa);
+// Function Declaration
+function buatObjeckMahasiswa(nama,npm,email,jurusan) {
+    var mhs = {};
+    mhs.nama = nama;
+    mhs.npm = npm;
+    mhs.email = email;
+    mhs.jurusan = jurusan;
+
+    return mhs;
+}
+
+var mhs3 = buatObjeckMahasiswa("Fanny","1174070","fanny@gmail.com","D4TI");
+
+// Constructer
+function Mahasiswa(nama,npm,email,jurusan) {
+    this.nama = nama;
+    this.npm = npm;
+    this.email = email;
+    this.jurusan = jurusan;
+}
+
+var mhs4 = new Mahasiswa("Fanny","1174070","fanny@gmail.com","D4TI");
